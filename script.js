9f9a26270c24d76455ba8e373976b02790149064
AidaFig <aidafigueiredo8@gmail.com>
2021-06-08 12:03:32 -0300

    regra para vencer adicionada

--- script.js
+++ script.js
@@ -1,76 +1,78 @@
+const casas = Array.from(document.querySelectorAll(".casa"));
 
-const board = document.querySelector(".board")
-const casas = document.getElementsByClassName("casas")
+let board = ["", "", "", "", "", "", "", "", ""];
+const winningCombos = [
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6],
+[1, 4, 7],
+[2, 5, 8],
+[0, 4, 8],
+[2, 4, 6]
+];
+let playerTurn = "X";
+let displayPlayer = document.getElementById("displayPlayer");
+displayPlayer.innerHTML = playerTurn;
 
-const player1 = "X"
-const player2 = "O"
-var playerTime = player2
-var gameOver = false;
+casas.forEach((casa) => {
+  // console.log(casa)
+  casa.addEventListener("click", handleClicked);
+});
 
-playersTurn()
-
-function playersTurn() {
-    if (gameOver) {
-        return
-    }
-    if (playerTime === player2) {
-        
-    var player = document.querySelectorAll("div.turn img")[0]
-    player.setAttribute("src", "./assets/img/cancel.png");
-    }else {
-        var player = document.querySelectorAll("div.turn img")[0]
-        player.setAttribute("src", "./assets/img/o.png")
-    }
+function handlePlayerTurn() {
+  if (playerTurn === "X") {
+    playerTurn = "O";
+  } else {
+    playerTurn = "X";
+  }
 }
 
+function handleClicked(e) {
+  // e.preventDefault()
+//   console.log(playerTurn);
+  // console.log(e.target.getAttribute("data-index"))
+  board[e.target.getAttribute("data-index")] = playerTurn;
 
-let jogadas = 0
-console.log(casas)
-for(var casa of casas){
-    casa.addEventListener("click", (evento) => {
-        // console.log(evento.target)
-        // console.log(jogadas)
-        jogadas++
-        const elementoSelecionado = evento.target
-        // console.log(elementoSelecionado)
-        
-        const img = document.createElement("img")
-        if (gameOver) {
-            return
-        }
-        if(jogadas % 2 === 0) { //true = par
-            img.setAttribute("src", "./assets/img/o.png")
-            img.setAttribute("class", "par")
-            playersTurn(playerTime = player2)
-            
-        } else { //false = impar
-            img.setAttribute("src", "./assets/img/cancel.png")
-            img.setAttribute("class", "impar")
-            playersTurn(playerTime = player1)
-        }
-        elementoSelecionado.appendChild(img)
-        console.log(elementoSelecionado.childElementCount)
-        // verificarVencedor(evento.target, casa)
-        // verificarVencedor(casa)
-        
-    })
+  const img = document.createElement("img");
+  e.target.appendChild(img);
+
+  if (playerTurn !== "X") {
+    //true = par
+    img.setAttribute("src", "./assets/img/o.png");
+    
+  } else {
+    //false = impar
+    img.setAttribute("src", "./assets/img/cancel.png");
+    
+  }
+
+  handlePlayerTurn();
+//   console.log(playerTurn);
+  displayPlayer.innerHTML = playerTurn;
+  // console.log(board[e.target.getAttribute("data-index")])
+  console.log(board);
+
+  let winner = winningConditions() 
+  console.log(winner)
+
+  e.target.removeEventListener("click", handleClicked)
+
 }
 
-function verificarVencedor(casa) {
-    if (jogadas === 9) {
-        
-        console.log(e)
+function winningConditions() {
+    
+    for (combo of winningCombos) {
+        const a = board[combo[0]]
+        const b = board[combo[1]]
+        const c = board[combo[2]]
+       
+
+        if(a === b && a === c && a != "") {
+            alert("GANHOU!!!")
+            return a
+        } 
+
     }
+
 }
-
-
-
-
-
-
-
-  // for(i = 0;i < casas.length; i++ ) {
-//     casas[i].addEventListener("click", (evento) => {
-//           console.log(evento.target)
-//     }, true)
-// }
